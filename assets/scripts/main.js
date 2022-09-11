e6892eb6f5792d6027eaed8867b875e9491c5880
Masi-M <masilakhemajiya@gmail.com>
2022-09-12 00:50:25 +0200

    init

---
+++ assets/scripts/main.js
@@ -0,0 +1,19 @@
+$('.img-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true, 
+    prevArrow: '',
+    nextArrow: '',
+  });
+
+$('.specialties').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    prevArrow: '',
+    nextArrow: '',
+  });
